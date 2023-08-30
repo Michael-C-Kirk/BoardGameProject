d0e875dd9b22eccefbba5dd0ac6f2d38750b3ca2
Michael Kirk <michael_c_kirk@yahoo.com>
2023-08-30 12:32:14 -0700

    Added Webdriver timeout exceptions and additional driver setup options

--- Code/populatedb.py
+++ Code/populatedb.py
@@ -2,20 +2,22 @@ from webscraper import WebScraper
 from getpass import getpass
 from mysql.connector import connect, Error
 
-w = WebScraper("https://boardgamegeek.com/boardgame/224517/brass-birmingham")
-username_list = w.get_usernames()
+if __name__ == "__main__":
+    w = WebScraper("https://boardgamegeek.com/boardgame/316554/dune-imperium")
+    username_list = w.get_usernames()
+    w.print_usernames()
 
-try:
-    with connect(host = "localhost", user = input("Enter username: "), password = getpass("Enter password: "), database = "boardgame_info",) as connection:
-        insert_users_query = """
-        INSERT IGNORE INTO users
-        (username)
-        VALUES (%s)
-        """
+    try:
+        with connect(host = "localhost", user = input("Enter username: "), password = getpass("Enter password: "), database = "boardgame_info",) as connection:
+            insert_users_query = """
+            INSERT IGNORE INTO users
+            (username)
+            VALUES (%s)
+            """
 
-        with connection.cursor() as cursor:
-            cursor.executemany(insert_users_query, username_list)
-            connection.commit()
+            with connection.cursor() as cursor:
+                cursor.executemany(insert_users_query, username_list)
+                connection.commit()
 
-except Error as e:
-    print(e)
+    except Error as e:
+        print(e)
