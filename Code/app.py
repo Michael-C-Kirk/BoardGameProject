b142a13c098c26783f9c712b2e0f76eff68088b8
MichaelDesktop <michael_c_kirk@yahoo.com>
2023-10-05 16:34:36 -0700

    merged difference between MichaelDesktop and Main

--- Code/app.py
+++ Code/app.py
@@ -1,17 +1,33 @@
 import configparser
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from db import DataBaseAppFunctionality
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
+from flask_mysqldb import MySQL
 
 config = configparser.ConfigParser()
 config.read("..\BoardGameProjectAdditonalFiles\config.ini")
-username, password = (config["credentials"]["username"], config["credentials"]["password"])
+username, password, host = (config["credentials"]["username"], config["credentials"]["password"], config["credentials"]["host"])
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = host
+app.config['MYSQL_USER'] = username
+app.config['MYSQL_PASSWORD'] = password
+app.config['MYSQL_DB'] = 'boardgame_info'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        SELECT board_games.id 
+        FROM boardgame_info.board_games 
+        WHERE board_games.name = "Nemesis";
+        """)
+    rv = cur.fetchall()
+    return str(rv)
+    #return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+
+    app.run(debug=True)
